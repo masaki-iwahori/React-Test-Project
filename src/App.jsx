1dcc5920e6d56f96e921d1a3016baf21e94c8c0b
岩堀聖輝 <m.iwahori@kramsMacBook.local>
2021-10-13 17:30:16 +0900

    [add]my-app2 added

---
+++ src/App.jsx
@@ -0,0 +1,28 @@
+import { useState, memo, useCallback } from "react";
+import { Child1 } from "./components/Child1";
+import { Child4 } from "./components/Child4";
+
+export const App = memo(() => {
+  console.log("Appレンダリング");
+
+  const [num, setNum] = useState(0);
+
+  const onClickButton = () => {
+    setNum(num + 1);
+  };
+
+  // 関数をメモ化
+  const onClickReset = useCallback(() => {
+    setNum(0);
+  }, []);
+
+  return (
+    <>
+      <button onClick={onClickButton}>ボタン</button>
+      <p>{num}</p>
+      {/* ↓Propsとして関数を設定 */}
+      <Child1 onClickReset={onClickReset} />
+      <Child4 />
+    </>
+  );
+});
